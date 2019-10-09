32bc913fae23d2ca2fdeff96eb99547e6f1737c1
Thomas Coughlin <tcoughlin@elon.edu>
2019-10-08 21:36:38 -0400

    rough draft without firebase hookup done

---
+++ assets/app.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+    var config = {
+
+
+    };
+    firebase.initializeApp(config);
+    var database = firebase.database();
+    var name;
+    var destination;
+    var firstTrain;
+    var frequency = 0;
+
+    $("#add-train").on("click", function() {
+        event.preventDefault();
+        // train ingo storage
+
+        name = $("#train-name").val().trim();
+        destination = $("#destination").val().trim();
+        firstTrain = $("#first-train").val().trim();
+        frequency = $("#frequency").val().trim();
+
+        // Pushing to db
+        database.ref().push({
+            name: name,
+            destination: destination,
+            firstTrain: firstTrain,
+            frequency: frequency,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+        $("form")[0].reset();
+    });
+
+    database.ref().on("child_added", function(childSnapshot) {
+        var nextArr;
+        var minAway;
+        //using moment for time
+        var firstTrainNew = moment(childSnapshot.val().firstTrain, "hh:mm").subtract(1, "years");
+        var diffTime = moment().diff(moment(firstTrainNew), "minutes");
+        var remainder = diffTime % childSnapshot.val().frequency;
+        var minAway = childSnapshot.val().frequency - remainder;
+        var nextTrain = moment().add(minAway, "minutes");
+        nextTrain = moment(nextTrain).format("hh:mm");
+
+        $("#add-row").append("<tr><td>" + childSnapshot.val().name +
+                "</td><td>" + childSnapshot.val().destination +
+                "</td><td>" + childSnapshot.val().frequency +
+                "</td><td>" + nextTrain + 
+                "</td><td>" + minAway + "</td></tr>");
+
+        });
+
+    database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+        // Print to page
+        $("#name-display").html(snapshot.val().name);
+        $("#email-display").html(snapshot.val().email);
+        $("#age-display").html(snapshot.val().age);
+        $("#comment-display").html(snapshot.val().comment);
+    });
+});
